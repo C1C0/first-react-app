64e36d917181a57bc92e577a7b476f05623ff4c2
C1C0 <kpskola1002@gmail.com>
2021-09-28 19:45:38 +0200

    Some of usage examples

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 // Functions approach
 function App() {
+  const name = "Kiko";
+  const x = true;
+
   return (
-    // Fragment
-    <> 
+    <div className="container">
       <h1>Hello React app</h1>
-      <h2>Hello</h2>
-    </>
+      <h2>Hello {name}</h2>
+      <h3>calculation: {1 + 2}</h3>
+      <h4>Ternary operator {x ? "True" : "False"}</h4>
+    </div>
   );
 }
 
