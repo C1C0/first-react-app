80c5a8a7c7f808c3008c624914f7568dfab11fa8
C1C0 <kpskola1002@gmail.com>
2021-09-28 19:53:01 +0200

    Example of Class approach

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import Header from "./components/Header";
+import React from "react";
 
-function App() {
-  return (
-    <div className="container">
-      <Header />
-    </div>
-  );
+// restart of the dev. server needed 
+class App extends React.Component {
+  render() {
+    return <>
+    <h1>Hello world 1</h1>
+    </>
+  }
 }
 
 export default App;
