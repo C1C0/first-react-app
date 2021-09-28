7d7a819109947c2e243e4894162a10d62676711e
C1C0 <kpskola1002@gmail.com>
2021-09-28 19:48:51 +0200

    tide up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 // Functions approach
 function App() {
-  const name = "Kiko";
-  const x = true;
-
   return (
     <div className="container">
       <h1>Hello React app</h1>
-      <h2>Hello {name}</h2>
-      <h3>calculation: {1 + 2}</h3>
-      <h4>Ternary operator {x ? "True" : "False"}</h4>
     </div>
   );
 }
