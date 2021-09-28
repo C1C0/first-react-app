b54a3feeab07628925657eaeaf741daa2d37a166
C1C0 <kpskola1002@gmail.com>
2021-09-28 19:42:32 +0200

    Only one return parent element - fix

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ function App() {
     // you can return only single parent element
     <div className="App">
       <h1>Hello React app</h1>
+      <h2>Hello</h2>
     </div>
-
-    // this will lead to error
-    <h2>Hello</h2>
   );
 }
 
