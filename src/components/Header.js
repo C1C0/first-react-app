e3fd95279b976361721a30b139c26a4814005333
C1C0 <kpskola1002@gmail.com>
2021-09-28 19:48:58 +0200

    Creating component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+// rafce - emet from vscode extension
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Task tracker</h1>
+        </header>
+    )
+}
+
+export default Header
+
