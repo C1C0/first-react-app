4e309ab0f992fdb540baa7a8a829b3285538afd9
C1C0 <kpskola1002@gmail.com>
2021-09-29 19:59:05 +0200

    Init DB

--- src/App.js
+++ src/App.js
@@ -5,26 +5,7 @@ import AddTask from "./components/AddTask";
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false);
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "blabal",
-      day: "Feb 5th at 2:30pm",
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: "Good tasks",
-      day: "Feb 8th at 1:30pm",
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: "Food shopping",
-      day: "Feb 7th at 6:30pm",
-      reminder: true,
-    },
-  ]);
+  const [tasks, setTasks] = useState([]);
 
   // Delete task
   const deleteTask = (id) => {
