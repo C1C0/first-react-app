1b8a8a2f8c3a2b6dcd0cc043b706d318587cfbab
C1C0 <kpskola1002@gmail.com>
2021-09-29 19:09:35 +0200

    Delete icons functions

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Tasks from "./components/Tasks";
 import { useState } from "react";
 
 function App() {
-  // IMUTABLE !!
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -25,10 +24,17 @@ function App() {
     },
   ]);
 
+  // Delete task 
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks}/>
+      {
+        tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} />) : "No Tasks To Show"
+      }
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,12 +1,12 @@
 import Task from "./Task";
 
-const Tasks = ({ tasks }) => {
+const Tasks = ({ tasks, onDelete }) => {
     return (
         <>
             {/* outputing  JSX in map() is called LIST*/}
             {/* Each LIST item should have unique key */}
             {tasks.map((task) => (
-                <Task key={task.id} task={task} />
+                <Task key={task.id} task={task} onDelete={onDelete} />
             ))}
         </>
     );
