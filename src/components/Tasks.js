673711dce365f5479b7a1a081a68c0edabce24f8
C1C0 <kpskola1002@gmail.com>
2021-09-29 19:20:44 +0200

    double clicking sets reminder

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,12 +1,12 @@
 import Task from "./Task";
 
-const Tasks = ({ tasks, onDelete }) => {
+const Tasks = ({ tasks, onDelete, onToggle }) => {
     return (
         <>
             {/* outputing  JSX in map() is called LIST*/}
             {/* Each LIST item should have unique key */}
             {tasks.map((task) => (
-                <Task key={task.id} task={task} onDelete={onDelete} />
+                <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle} />
             ))}
         </>
     );
